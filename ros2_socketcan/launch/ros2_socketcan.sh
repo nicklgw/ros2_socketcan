d22730909769b0ace01e1d76acfc5600bc4d2c22
liguowang <liguowang01@countrygarden.com.cn>
2024-08-19 14:54:54 +0800

    use rmw_cyclonedds_cpp

--- ros2_socketcan/launch/ros2_socketcan.sh
+++ ros2_socketcan/launch/ros2_socketcan.sh
@@ -2,8 +2,11 @@
 
 export ROS_DOMAIN_ID=5
 source /opt/ros/humble/setup.bash
+export ROS_LOCALHOST_ONLY=1
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 mkdir -m a=rwx -p /var/log/bzlrobot/ros2_socketcan
 export ROS_LOG_DIR="/var/log/bzlrobot/ros2_socketcan"
 
 /usr/bin/python3 /opt/ros/humble/bin/ros2 launch ros2_socketcan socket_can_bridge.launch.xml interface:=can1
+
